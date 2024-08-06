b8cbd35ba169da80d6a332efa0aa510355ba3468
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2024-08-05 21:44:27 -0400

    laptop checkbox & workingProperty bug fix

--- scripts/resource.js
+++ scripts/resource.js
@@ -1,13 +1,7 @@
 console.log('resource.js')
 
 $('#laptop [type=checkbox]').on('change', () => {
-    thisName = '#laptop [type=checkbox]';
-    if ($(thisName).is(':checked')) { // 此勾选盒勾选的情况
-        $('#model-display [type=checkbox]').not(thisName).prop('checked', false); // 选择所有其他勾选盒，取消勾选
-        gameData.workingProperty = 'laptop'; // 办公了就不能干其他工作
-    } else { // 此勾选盒不勾选的情况
-        gameData.workingProperty = '';
-    }
+    checkBoxEvent('laptop');
 })
 
 $('#mini-truck [type=decr]').click(() => {
@@ -86,22 +80,26 @@ function checkBoxEvent(propertyName) {
     // console.log(gameData.propertyList[propertyName])
     thisName = `#${propertyName} [type=checkbox]`;
     if ($(thisName).is(':checked')) { // 此勾选盒勾选的情况
+
         $('#model-display [type=checkbox]').not(thisName).prop('checked', false); // 选择所有其他勾选盒，取消勾选
-        if ( gameData.workingProperty == 'laptop' || // 单独给laptop更新用
+        // console.log(gameData.workingProperty)
+        if ( propertyName == 'laptop' || // 单独给laptop更新用
             gameData.propertyList[propertyName].amount > gameData.propertyList[propertyName].amountUsed) { // 确保有空余资产给小人自己用
             // 根据尚未被改变的gameData.workingProperty，判断上一个被使用的资产是什么
-            if (gameData.propertyList[gameData.workingProperty] !== undefined) {
-                gameData.propertyList[gameData.workingProperty].amountUsed--;
-            }
-            gameData.propertyList[propertyName].amountUsed++;
+            if (gameData.propertyList[gameData.workingProperty] !== undefined) gameData.propertyList[gameData.workingProperty].amountUsed--;
+            if (gameData.propertyList[propertyName] !== undefined) gameData.propertyList[propertyName].amountUsed++;
             gameData.workingProperty = propertyName; // gameData.workingProperty更新
         } else { // 否则取消勾选此勾选盒
             $(thisName).prop('checked', false);
+            gameData.workingProperty = '';
         }
         // console.log(gameData.propertyList[propertyName])
+
     } else { // 此勾选盒不勾选的情况
-        gameData.propertyList[propertyName].amountUsed--;
+
+        if (gameData.propertyList[propertyName] !== undefined) gameData.propertyList[propertyName].amountUsed--;
         gameData.workingProperty = ''
         // console.log(gameData.propertyList[propertyName])
+
     }
 }
